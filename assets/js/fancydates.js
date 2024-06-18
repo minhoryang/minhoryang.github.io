4208eddee97a57833e32f67d2949d2df8573b564
Minho Ryang <minho@comcom.ai>
2024-06-18 10:53:00 +0900

    [Chore] ga4 and gtags

--- assets/js/fancydates.js
+++ assets/js/fancydates.js
@@ -1,19 +1,21 @@
-function fancydates(fanciness, date_format) {
-    if (fanciness == 0) {
+function fancydates(fanciness, luxonDateFormat) {
+    if (fanciness === 0) {
         return;
     }
 
-    var dates = document.querySelectorAll('.dt-published, .dt-updated');
+    var dates = document.querySelectorAll('.dt-published, .dt-updated, .listdate');
 
     var l = dates.length;
 
     for (var i = 0; i < l; i++) {
-        var d = moment(dates[i].attributes.datetime.value);
+        var d = luxon.DateTime.fromISO(dates[i].attributes.datetime.value);
         var o;
-        if (fanciness == 1) {
-            o = d.local().format(date_format);
+        if (fanciness === 1 && luxonDateFormat.preset) {
+            o = d.toLocal().toLocaleString(luxon.DateTime[luxonDateFormat.format]);
+        } else if (fanciness === 1) {
+            o = d.toLocal().toFormat(luxonDateFormat.format);
         } else {
-            o = d.fromNow();
+            o = d.toRelative();
         }
         dates[i].innerHTML = o;
     }
